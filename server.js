09fa5074e9a964fa1f782c07418e9d52ed0f0853
Christopher Bruder <cbruder1292@gmail.com>
2020-05-04 11:22:00 -0500

    set up server and said hello

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var exphbs = require("express-handlebars");
+// const bookController = require('./controller/bookController');
+
+const db = require('./models');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.get("*" , function(req, res) {
+    res.send("Hello");
+})
+
+
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+    console.log('Server listening on: http://localhost:' + PORT);
+    });
+})
