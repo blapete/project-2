73f31cb8fcbae817a4d55fe9fcd33210b882414b
CJPALME <cjpalmerin@gmail.com>
2020-05-06 14:54:15 -0500

    added POST routes for admin

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const spotControl = require('../controller/spotcontroller');
+const db = require("../models");
 
 
 
@@ -9,10 +10,10 @@ router.route('/')
     .get(function (req, res) {
         // var data = { info: [{ info: "this is the info" }] }
         console.log("Heyy")
-        spotControl.getAllSpots().then(data => {
-            // console.log(data)
-            res.render("index", { spots: data });
-        })
+        // spotControl.getAllSpots().then(data => {
+        //     // console.log(data)
+        //     res.render("index", { spots: data });
+        // })
     })
 
 router.route('/favorites')
@@ -29,9 +30,13 @@ router.route('/favorites')
 router.route('/admin312')
     .get(function( req, res) {
         res.render("admin");
-
-
     })
+
+router.post('/api/admin312', function(req, res) {
+    db.Spot.create(req.body).then(function(dbSpot) {
+      res.json(dbSpot);
+    });
+});
 
 // no routes are hit catchall
 // router.use(function (req, res) {
